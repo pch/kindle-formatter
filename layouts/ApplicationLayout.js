3153909e9daa30a34831f446eb0c45e4059c06a6
Piotr Chmolowski <piotr@chmolowski.pl>
2020-10-29 16:12:35 +0100

    Emoji favicon

--- layouts/ApplicationLayout.js
+++ layouts/ApplicationLayout.js
@@ -14,6 +14,11 @@ export default function ApplicationLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Fira+Mono&family=Inter:wght@400;600&display=swap"
           rel="stylesheet"
         />
+
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📚</text></svg>"
+        />
       </Head>
 
       <PageHeader />
